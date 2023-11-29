4d155f3743fdb4e253584fa7685e2e46a2bf1d46
eugene <tashoglo00@mail.ru>
2023-11-29 09:27:33 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Абстрактное выражение
+interface IExpression
+{
+    int Interpret(Dictionary<string, int> context);
+}
+
+// Конкретное выражение
+class NumberExpression : IExpression
+{
+    private int _number;
+
+    public NumberExpression(int number)
+    {
+        _number = number;
+    }
+
+    public int Interpret(Dictionary<string, int> context)
+    {
+        return _number;
+    }
+}
+
+// Конкретное выражение для сложения
+class AddExpression : IExpression
+{
+    private IExpression _leftExpression;
+    private IExpression _rightExpression;
+
+    public AddExpression(IExpression left, IExpression right)
+    {
+        _leftExpression = left;
+        _rightExpression = right;
+    }
+
+    public int Interpret(Dictionary<string, int> context)
+    {
+        return _leftExpression.Interpret(context) + _rightExpression.Interpret(context);
+    }
+}
+
+// Пример использования
+class Program
+{
+    static void Main()
+    {
+        // Создаем контекст
+        Dictionary<string, int> context = new Dictionary<string, int>();
+        context.Add("a", 5);
+        context.Add("b", 10);
+
+        // Создаем выражения
+        IExpression expression = new AddExpression(new NumberExpression(2), new NumberExpression(3));
+
+        // Интерпретируем выражение
+        int result = expression.Interpret(context);
+
+        // Выводим результат
+        Console.WriteLine("Результат: " + result);
+    }
+}
